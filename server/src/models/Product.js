d50811c938d49ff3d675c38638cdb4a39982055c
Tama <mytamhuynhhmt@gmail.com>
2025-09-04 23:04:58 +0700

    8 sản phẩm mới nhất, 6 sản phẩm bán chạy, 8 sản phẩm xem nhiều nhất, 4 sản phẩm khuyến mãi cao nhất

---
+++ server/src/models/Product.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema({
+  name: { 
+    type: String, 
+    required: true 
+},
+  description: {
+    type: String
+},
+  price: { 
+    type: Number, 
+    required: true 
+},
+  images: [String],
+  stock: { 
+    type: Number,
+    default: 0
+},
+  category: {
+    String
+},
+  sold: {
+    type: Number,
+    default: 0
+},
+  views: {
+    type: Number,
+    default: 0
+},
+  discount: {
+    type: Number,
+    default: 0
+} // %
+}, { timestamps: true });
+
+const Product = mongoose.model("Product", productSchema);
+
+module.exports = Product;
